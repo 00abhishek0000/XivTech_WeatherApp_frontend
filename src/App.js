f0bbb2f1635fafaad1efdf59e6944772b0a84132
00abhishek0000 <batishabhishek07@gmail.com>
2024-04-23 15:19:59 +0530

    majboori

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import WeatherForm from './components/weatherForm.js';
 import WeatherDisplay from './components/Display.js';
+import LoginButton from './LoginButton.js';
+import LogoutButton from './LogoutButton.js';
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
   const [weatherData, setWeatherData] = useState({});
@@ -9,13 +12,18 @@ function App() {
   const handleWeatherData = (data) => {
     setWeatherData(data);
   };
+  const {isAuthenticated } = useAuth0();
 
   return (
+    <>
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherForm onWeatherData={handleWeatherData} />
-      <WeatherDisplay weatherData={weatherData} />
+      <LoginButton/>
+      <LogoutButton/>
+      {isAuthenticated &&
+        <><h1>Weather App</h1><WeatherForm onWeatherData={handleWeatherData} /><WeatherDisplay weatherData={weatherData} /></>
+      }
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Auth0Provider,useAuth0} from '@auth0/auth0-react'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider domain='dev-eigkjcuzp0m8wbbl.us.auth0.com' clientId='uLhsmLRkDWLB484bjzKc5nZeyQnPIYbc' redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
